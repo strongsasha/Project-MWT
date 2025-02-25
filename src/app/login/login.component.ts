adfe2d92656274bb288029bb9d3a216b4d4fefa5
Peter Gurský <peter.gursky@upjs.sk>
2025-02-25 13:10:10 +0100

    login with errorMessage

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,18 +1,38 @@
 import { Component, inject } from '@angular/core';
 import { Auth } from '../../entities/auth';
 import { UsersService } from '../../services/users.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   usersService = inject(UsersService);
   auth = new Auth("Peter", "sovy");
+  hide = true;
+  errorMessage = '';
 
   login() {
-    this.usersService.login(this.auth).subscribe(token => console.log('token', token));
+    this.usersService.login(this.auth).subscribe({
+      next: success => {
+        if (success) {
+          // choď na extended users
+        } else {
+          // vypíš používateľovi že zadal zlé heslo alebo meno
+          this.errorMessage = "Wrong name or password";
+        }
+      },
+      error: err => {
+        this.errorMessage = "Server not available";
+      }
+    });
   }
 }
--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { User } from '../entities/user';
-import { map, Observable, of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { catchError, map, Observable, of, tap, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Auth } from '../entities/auth';
 
 @Injectable({
@@ -13,6 +13,7 @@ export class UsersService {
            new User("MarienkaService", "marienka@janko.sk", 2, new Date('2025-01-01')),
            {name:"JožkoService", email: "jozko@janko.sk", password: "heslo"}
   ];
+  token = '';
   
   getUsersSynchronous():User[] {
     return this.users;
@@ -28,7 +29,18 @@ export class UsersService {
     );
   }
 
-  login(auth:Auth): Observable<string> {
-    return this.http.post('http://localhost:8080/login',auth, {responseType: 'text'})
+  login(auth:Auth): Observable<boolean> {
+    return this.http.post('http://localhost:8080/login',auth, {responseType: 'text'}).pipe(
+      tap(token => this.token = token),
+      map(token => true),
+      catchError(error => {
+        if (error instanceof HttpErrorResponse) {
+          if (error.status == 401) {
+            return of(false);
+          }
+        }
+        return throwError(() => error);
+      })
+    );
   }
 }
