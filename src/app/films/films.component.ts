d4101e7ee5400fb0479c810d14d62700dbf472d1
Peter Gurský <peter.gursky@upjs.sk>
2025-03-25 14:49:35 +0100

    server side pagination, sort, filter

--- src/app/films/films.component.ts
+++ src/app/films/films.component.ts
@@ -1,23 +1,98 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { AfterViewInit, Component, computed, effect, inject, OnInit, signal, viewChild } from '@angular/core';
 import { MaterialModule } from '../../modules/material.module';
 import { Film } from '../../entities/film';
 import { FilmsService } from '../../services/films.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { rxResource, toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { switchMap, tap } from 'rxjs';
+import { L, P, Q } from '@angular/cdk/keycodes';
+import { MatSort } from '@angular/material/sort';
+import { UsersService } from '../../services/users.service';
+import { MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-films',
-  imports: [MaterialModule],
+  imports: [MaterialModule, MatProgressSpinnerModule],
   templateUrl: './films.component.html',
   styleUrl: './films.component.css'
 })
-export default class FilmsComponent implements OnInit{
+export default class FilmsComponent implements AfterViewInit {
   filmsService = inject(FilmsService);
-  columnsToDisplay = ['id', 'nazov', 'rok'];
-  films: Film[] = [];
+  usersService = inject(UsersService);
 
-  ngOnInit(): void {
-      this.filmsService.getFilms('slovenskyNazov',false, 0, 5).subscribe(filmsResponse => {
-          this.films = filmsResponse.items;
-          console.log('filmsResponse', filmsResponse);
-        });
+  columnsToDisplayS = computed(() => this.usersService.loggedUserS() 
+                   ? ['id', 'nazov', 'rok','slovenskyNazov', 'afi1998', 'afi2007']
+                   : ['id', 'nazov', 'rok']);
+  paginatorS = viewChild.required<MatPaginator>(MatPaginator);
+  sortHeaderS = viewChild.required<MatSort>(MatSort);
+
+  orderByS = signal<string | undefined>(undefined);
+  descendingS = signal<boolean | undefined>(undefined);
+  indexFromS = signal<number | undefined>(0);
+  indexToS = signal<number | undefined>(5);
+  searchS = signal<string | undefined>(undefined);
+
+  queryS = computed(() => new Query(this.orderByS(), 
+                                    this.descendingS(), 
+                                    this.indexFromS(), 
+                                    this.indexToS(),
+                                    this.searchS()
+                                  ));
+
+  // response$ = toObservable(this.queryS).pipe(
+  //   tap(query => console.log("request:", query)),
+  //   switchMap(query => this.filmsService.getFilms(query.orderBy, query.descending, query.indexFrom, query.indexTo, query.search))
+  // );
+
+  // responseS = toSignal(this.response$);
+  filmsResource = rxResource({
+    request: () => this.queryS(),
+    loader: ({request: query}) => this.filmsService.getFilms(query.orderBy, query.descending, query.indexFrom, query.indexTo, query.search)
+  });
+
+  responseS = this.filmsResource.value;
+  filmsS = computed(() => this.responseS()?.items || []);
+
+  constructor() {
+    effect(() => console.log(this.responseS()));
   }
+
+  ngAfterViewInit(): void {
+    this.paginatorS().page.subscribe(pageEvent => {
+      console.log("Page event", pageEvent);
+      this.indexFromS.set(pageEvent.pageIndex * pageEvent.pageSize);
+      this.indexToS.set(Math.min((pageEvent.pageIndex + 1) * pageEvent.pageSize, 
+                                 pageEvent.length));
+    });
+    this.sortHeaderS().sortChange.subscribe(sortEvent => {
+      console.log('Sort header event', sortEvent);
+      if (sortEvent.direction === "") {
+        this.descendingS.set(undefined);
+        this.orderByS.set(undefined);
+        return;
+      }
+      this.descendingS.set(sortEvent.direction === "desc");
+      let column = sortEvent.active;
+      if (column === 'afi1998') column='poradieVRebricku.AFI 1998';
+      if (column === 'afi2007') column='poradieVRebricku.AFI 2007';
+      this.orderByS.set(column);
+      this.paginatorS().firstPage();
+    });
+  }
+
+  filter(event: any) {
+    const filter = (event.target.value as string).trim().toLowerCase();
+    this.searchS.set(filter);
+    this.paginatorS().firstPage();
+  }
+}
+
+class Query {
+  constructor(
+    public orderBy?: string,
+    public descending?: boolean,
+    public indexFrom?: number,
+    public indexTo?: number,
+    public search?: string
+  ) {}
 }
